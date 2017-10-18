4d9e246824ce451e57abbf25e06c4d16cefb7424
panshuai <348539627@qq.com>
2017-10-18 12:22:52 +0000

    finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@yield('title','全民舞动')--健身，舞动嗨起来</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','全民舞动')
+@section('content')
+<h1>主页</h1>
+@stop
